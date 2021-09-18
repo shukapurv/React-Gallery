f16e2c4e3267f6f0a626f6590751704feaab20e6
chirayumit001 <84196347+chirayumit001@users.noreply.github.com>
2021-09-18 17:16:02 +0530

    Add and Configure Sass modules (#5)
    - chore: Integrate sass
    - chore: Add config for sass

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
-import '../styles/globals.css'
+import '../styles/globals.scss'
 import type { AppProps } from 'next/app'
-
+/* eslint-disable react/jsx-props-no-spreading */
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
 }
